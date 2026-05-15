5daa31a2bfc7c0a75c7791ba233ce9b75d4a697e
DevGiu <devgiudev@gmail.com>
2026-05-15 10:10:26 +0200

    Styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://devgiudev.github.io",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-default",
+      wrap: true,
+    },
+  },
 });
